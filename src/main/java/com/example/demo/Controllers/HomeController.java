80192c02f7ee253a56a7b60f420a6307c4f54fd6
usharani4 <81891490+usharani4@users.noreply.github.com>
2021-09-30 17:57:35 +0530

    errors rectified

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -2,15 +2,18 @@ package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String index() {
+		System.out.println("testing");
 		return "Index";
 	}
 	@RequestMapping("/quiz")
 	public String quiz() {
+		System.out.println("testing");
 		return "Quiz";
 }
 }
